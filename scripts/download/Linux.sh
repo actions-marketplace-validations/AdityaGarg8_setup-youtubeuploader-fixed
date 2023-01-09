81c6deff5d6822edd9aaddde81f0457927d91165
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-01-09 17:58:20 +0530

    Update Linux.sh

--- scripts/download/Linux.sh
+++ scripts/download/Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-version=$(curl -s $GITHUB_API_URL/repos/porjo/youtubeuploader/releases/latest | jq -r .tag_name)
+version=23.01
 echo ::group::Downloading youtubeuploader $version for Linux
 aria2c -x 16 $GITHUB_SERVER_URL/porjo/youtubeuploader/releases/download/$version/youtubeuploader_${version}_Linux_x86_64.tar.gz
 mkdir youtubeuploader
